740704453482a8d9e4948b9a2e6173cec01144b6
bminnich3 <48356785+bminnich3@users.noreply.github.com>
2019-05-30 08:43:33 -0400

    Create InventoryList.java

---
+++ InventoryList.java
@@ -0,0 +1,64 @@
+import java.util.ArrayList;
+
+public class InventoryList implements Listable
+{
+    public InventoryList()
+    {
+      items = new ArrayList<String>();
+    }
+    
+    // interesting methods
+
+    public void displayList()
+    {
+      for (String nextItem : items)
+      {
+        System.out.print(" " + nextItem);
+      }
+
+      System.out.println();
+    }
+
+    public int numberOfItems()
+    {
+      return items.size();
+    }
+
+    // required methods because of implemented interfaces
+
+    public String selectItem(int pos)
+    {
+      return items.get(pos);
+    }
+
+    public boolean removeItem(String item)
+    {
+      if (items.indexOf(item) >= 0)
+      {
+        items.remove(item);
+        return true;
+      }
+
+      return false;
+    }
+
+    public void addItem(String newItem)
+    {
+      items.add(newItem);
+    }
+
+
+    public boolean containsItem(String item)
+    {
+      if (items.indexOf(item) >= 0)
+      {
+        return true;
+      }
+
+      return false;
+    }
+
+    // instance variables
+
+    ArrayList<String> items; 
+}
